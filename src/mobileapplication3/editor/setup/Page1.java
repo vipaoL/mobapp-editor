e47188fea117197b5c4a6b73208719f18d61c669
vipoll <vipollmail@gmail.com>
2024-08-16 23:14:02 +0400

    add app icon

--- src/mobileapplication3/editor/setup/Page1.java
+++ src/mobileapplication3/editor/setup/Page1.java
@@ -5,18 +5,20 @@
  */
 package mobileapplication3.editor.setup;
 
-import javax.microedition.lcdui.Graphics;
+import java.io.IOException;
+
+import javax.microedition.lcdui.Image;
+
 import mobileapplication3.editor.ui.Button;
 import mobileapplication3.editor.ui.IUIComponent;
-import mobileapplication3.editor.ui.UIComponent;
+import mobileapplication3.editor.ui.ImageComponent;
+import mobileapplication3.editor.ui.TextComponent;
 
 /**
  *
  * @author vipaol
  */
 public class Page1 extends AbstractSetupWizardPage {
-    
-    private UIComponent logo;
     
     public Page1(Button[] buttons, SetupWizard.Feedback feedback) {
         super("Welcome to the structure editor for mobapp-game", buttons, feedback);
@@ -28,27 +30,29 @@ public class Page1 extends AbstractSetupWizardPage {
     }
 
     protected IUIComponent initAndGetPageContent() {
-        logo = new UIComponent() {
-            public void onPaint(Graphics g, int x0, int y0, int w, int h, boolean forceInactive) {
-                g.setColor(0x0000ff);
-                g.drawArc(x0, y0, w-1, h-1, 0, 360);
-                g.drawString("Here will be", x0 + w/2, y0 + h/2, Graphics.BOTTOM | Graphics.HCENTER);
-                g.drawString("a cool logo.", x0 + w/2, y0 + h/2, Graphics.TOP | Graphics.HCENTER);
-            }
-            
-            public boolean canBeFocused() {
-                return false;
-            }
-            
-            public boolean handlePointerReleased(int x, int y) {
-                return false;
-            }
-            
-            public boolean handleKeyPressed(int keyCode, int count) {
-                return false;
-            }
-        };
-        return logo;
+    	Image logo = null;
+    	String errorMessage = null;
+    	
+        try {
+        	logo = Image.createImage("/logo.png");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			errorMessage = ex + " ";
+			try {
+	        	logo = Image.createImage("/icon.png");
+			} catch (IOException e) {
+				e.printStackTrace();
+				errorMessage += e;
+			}
+		}
+        
+        if (logo != null) {
+        	return new ImageComponent(logo).setBgColor(COLOR_ACCENT_MUTED);
+        } else {
+			return new TextComponent("Could not load logo image: " + errorMessage)
+					.setFontColor(0xff0000)
+					.setBgColor(0);
+		}
     }
 
     public void setPageContentBounds(IUIComponent pageContent, int x0, int y0, int w, int h) {
