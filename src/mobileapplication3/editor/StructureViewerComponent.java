08ec8b53cdd7648f03cd39ced7e00122dca74b49
vipoll <vipollmail@gmail.com>
2024-08-17 06:13:09 +0400

    better zoom and center structures in viewer

--- src/mobileapplication3/editor/StructureViewerComponent.java
+++ src/mobileapplication3/editor/StructureViewerComponent.java
@@ -5,6 +5,7 @@ import javax.microedition.lcdui.Graphics;
 import mobileapplication3.editor.ui.UIComponent;
 import mobileapplication3.elements.Element;
 import mobileapplication3.elements.EndPoint;
+import mobileapplication3.elements.StartPoint;
 import mobileapplication3.utils.Mathh;
 
 public class StructureViewerComponent extends UIComponent { // TODO: merge with EditorCanvas
@@ -12,15 +13,19 @@ public class StructureViewerComponent extends UIComponent { // TODO: merge with 
 	private static final int MIN_ZOOM_OUT = 8;
 	private static final int MAX_ZOOM_OUT = 200000;
 	
-	 private int colLandscape = 0x4444ff;
+	private int colLandscape = 0x4444ff;
     private int colBody = 0xffffff;
     private int offsetX, offsetY;
 	private int zoomOut = 8192;
+	short start;
+	short end;
 	
 	private Element[] elements;
 	
 	public StructureViewerComponent(Element[] elements) {
 		this.elements = elements;
+		start = StartPoint.findStartPoint(elements)[0];
+		end = elements[0].getArgsValues()[0];
 		setBgColor(COLOR_ACCENT_MUTED);
 		System.out.println("structure viewer: got " + elements.length + "elements");
 	}
@@ -62,9 +67,10 @@ public class StructureViewerComponent extends UIComponent { // TODO: merge with 
 	
 	protected void onSetBounds(int x0, int y0, int w, int h) {
 		zoomOut = Mathh.constrain(MIN_ZOOM_OUT, 4000000 / Math.min(w, h), MAX_ZOOM_OUT);
+		zoomOut = Math.max(zoomOut, 1000*(end - start)*3/w/2);
 		offsetX = w/2;
 		try {
-			offsetX -= elements[0].getArgsValues()[0] / 2 * 1000 / zoomOut;
+			offsetX -= (end + start) / 2 * 1000 / zoomOut;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
