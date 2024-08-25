adef06ce6fc897f408ec55baa5192f42c07004bc
vipoll <vipollmail@gmail.com>
2024-08-26 03:25:33 +0400

    Split the sources into libraries (submodules in ./lib)
    I splitted the sources into libraries to make developing a crossplatform app easier. There are j2me and android versions, a desktop port is planned

--- src/mobileapplication3/ui/UISettings.java
+++
@@ -1,7 +0,0 @@
-package mobileapplication3.ui;
-
-public interface UISettings {
-	public abstract boolean getAnimsEnabled();
-	public abstract boolean getKeyRepeatedInListsEnabled();
-	public abstract void onChange();
-}
